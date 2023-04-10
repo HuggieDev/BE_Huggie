dbb6474231be9215970f0fc2c11fa4f85e5295fe
쿼카 <42869930+grin-quokka@users.noreply.github.com>
2023-04-10 18:36:49 +0900

    🚧 validator 적용중

--- src/apis/reviews/dto/createReview.dto.ts
+++ src/apis/reviews/dto/createReview.dto.ts
@@ -1,10 +1,21 @@
 import { Review } from '../entities/review.entity'
-import { PickType } from '@nestjs/mapped-types'
+import { IntersectionType, PickType } from '@nestjs/mapped-types'
+import { IsNotEmpty } from 'class-validator'
+import { CreateStoreInput } from 'src/apis/stores/dto/createStore.dto'
 
 export class CreateReviewInput extends PickType(Review, [
     'contents',
     'score',
     'visitDate',
 ] as const) {
+    @IsNotEmpty()
     userId: string
+}
+
+export class CreateReviewWithStore {
+    // createReviewInput: CreateReviewInput
+    // createStoreInput: CreateStoreInput
+    aa: {
+        bb: string
+    }
 }
