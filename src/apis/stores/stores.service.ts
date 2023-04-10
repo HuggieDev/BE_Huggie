cbc7a2e7ea57b42626dba6570a18691deed109ea
grin-quokka <grinquokka@gmail.com>
2023-04-10 18:36:50 +0900

    :necktie: store의 생성, 조회 로직 추가

---
+++ src/apis/stores/stores.service.ts
@@ -0,0 +1,21 @@
+import { InjectRepository } from '@nestjs/typeorm'
+import { Store } from './entities/store.entity'
+import { Injectable } from '@nestjs/common'
+import { FindOptionsWhere, Repository } from 'typeorm'
+import { CreateStoreInput } from './dto/createStore.dto'
+
+@Injectable()
+export class StoresService {
+    constructor(
+        @InjectRepository(Store)
+        private readonly storeRepository: Repository<Store>
+    ) {}
+
+    async createStore(createInput: CreateStoreInput): Promise<Store> {
+        return await this.storeRepository.save(createInput)
+    }
+
+    async findOneWithInfo(info: FindOptionsWhere<Store>): Promise<Store> {
+        return await this.storeRepository.findOne({ where: { ...info } })
+    }
+}
