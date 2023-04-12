69ebce3e1f2ca84f4f41df81c1e6907c23691c33
Daseul1 <chlektmf4411@gmail.com>
2023-04-12 20:42:42 +0900

    :construction: user swagger 추가

--- src/apis/auth/auth.controller.ts
+++ src/apis/auth/auth.controller.ts
@@ -3,12 +3,27 @@ import { AuthGuard } from '@nestjs/passport'
 import { Request, Response } from 'express'
 import { AuthService } from './auth.service'
 import { IOAuthUser } from './interfaces/auth.interface'
+import {
+    ApiBearerAuth,
+    ApiCookieAuth,
+    ApiOperation,
+    ApiResponse,
+    ApiUnauthorizedResponse,
+} from '@nestjs/swagger'
 
 @Controller()
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
+    // @ApiBearerAuth()
+    @ApiCookieAuth()
     @Get('social')
+    @ApiOperation({
+        summary: '소셜 로그인 API',
+        description: '소셜 로그인을 한다.',
+    })
+    @ApiResponse({ status: 200, description: '소셜 로그인 성공' })
+    @ApiResponse({ status: 400, description: '소셜 로그인 실패' })
     @UseGuards(AuthGuard('kakao'))
     async loginKakao(
         @Req() req: Request & IOAuthUser, //
--- src/apis/users/entities/user.entity.ts
+++ src/apis/users/entities/user.entity.ts
@@ -5,15 +5,24 @@ import {
     PrimaryGeneratedColumn,
 } from 'typeorm'
 import { USER_SOCIAL_TYPE_ENUM } from '../types/user.type'
+import { ApiProperty, ApiResponse } from '@nestjs/swagger'
 
 @Entity()
 export class User {
     @PrimaryGeneratedColumn('uuid')
     id: string
 
+    @ApiProperty({
+        example: '허기져',
+        description: '사용자 닉네임',
+    })
     @Column()
     nickName: string
 
+    @ApiProperty({
+        example: 'huggie@kakao.com',
+        description: '사용자 이메일',
+    })
     @Column()
     email: string
 
