ab19d96bb6dce059b9728d3f10423d656b9d66ea
Daseul1 <chlektmf4411@gmail.com>
2023-04-12 20:42:42 +0900

    :construction: review controller swagger docs 추가

--- src/apis/reviews/reviews.controller.ts
+++ src/apis/reviews/reviews.controller.ts
@@ -2,12 +2,21 @@ import { Body, Controller, Post } from '@nestjs/common'
 import { ReviewsService } from './reviews.service'
 import { CreateReviewWithStore } from './dto/createReview.dto'
 import { Review } from './entities/review.entity'
+import { ApiOperation, ApiResponse } from '@nestjs/swagger'
 
 @Controller('review')
 export class ReviewsController {
     constructor(private readonly reviewsService: ReviewsService) {}
 
     @Post()
+    @ApiOperation({
+        summary: '리뷰 생성 API',
+    })
+    @ApiResponse({
+        status: 201,
+        description: '리뷰 생성 성공.',
+        type: Review,
+    })
     createReview(
         @Body() createReviewWithStore: CreateReviewWithStore
     ): Promise<Review> {
