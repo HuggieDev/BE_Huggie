ed6694f0e2073ad04ac17297256e4ad3b05b0db0
Daseul1 <chlektmf4411@gmail.com>
2023-04-13 18:12:52 +0900

    :memo: createReview input swagger docs 추가

--- src/apis/reviews/dto/createReview.dto.ts
+++ src/apis/reviews/dto/createReview.dto.ts
@@ -10,21 +10,34 @@ import {
 } from 'class-validator'
 import { CreateStoreInput } from 'src/apis/stores/dto/createStore.dto'
 import { Type } from 'class-transformer'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class CreateReviewInput extends PickType(Review, [
     'contents',
     'score',
     'visitDate',
 ] as const) {
+    @ApiProperty({
+        example: '0dc011aa-d76e-11ed-afa1-0242ac120002',
+        required: true,
+    })
     @IsUUID()
     @IsNotEmpty()
     userId: string
 
+    @ApiProperty({
+        type: '[string]',
+        example: ['url1', 'url2'],
+    })
     @IsOptional()
     @IsArray()
     @IsString({ each: true })
     imgs: string[]
 
+    @ApiProperty({
+        type: '[string]',
+        example: ['해장국', '수육'],
+    })
     @IsOptional()
     @IsArray()
     @IsString({ each: true })
