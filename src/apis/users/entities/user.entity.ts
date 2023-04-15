8fefbe46335107f83264b0e2975d051571298ac1
Daseul1 <chlektmf4411@gmail.com>
2023-04-15 15:37:25 +0900

    :safety_vest: user entity validation 추가

--- src/apis/users/entities/user.entity.ts
+++ src/apis/users/entities/user.entity.ts
@@ -6,9 +6,11 @@ import {
 } from 'typeorm'
 import { USER_SOCIAL_TYPE_ENUM } from '../types/user.type'
 import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator'
 
 @Entity()
 export class User {
+    @IsUUID()
     @PrimaryGeneratedColumn('uuid')
     id: string
 
@@ -16,9 +18,13 @@ export class User {
         example: '허기져',
         description: '사용자 닉네임',
     })
+    @IsNotEmpty()
+    @IsString()
     @Column()
     nickName: string
 
+    @IsNotEmpty()
+    @IsString()
     @ApiProperty({
         example: 'huggie@kakao.com',
         description: '사용자 이메일',
