54b50baf1ae5d761f18e62c451fb01b0355989a7
grin-quokka <grinquokka@gmail.com>
2023-05-16 21:47:57 +0900

    :bug: 빠져야하는 인풋

--- src/apis/stores/dto/createStore.dto.ts
+++ src/apis/stores/dto/createStore.dto.ts
@@ -1,4 +1,7 @@
 import { OmitType } from '@nestjs/swagger'
 import { Store } from '../entities/store.entity'
 
-export class CreateStoreInput extends OmitType(Store, ['id'] as const) {}
+export class CreateStoreInput extends OmitType(Store, [
+    'id',
+    'reviews',
+] as const) {}
