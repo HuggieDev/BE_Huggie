91e4ad9ed57cfd342b8d44d41333af2faab9ef6c
grin-quokka <grinquokka@gmail.com>
2023-05-24 09:08:48 +0900

    :necktie: fetchReviews 컨트롤러만 작성

--- src/apis/reviews/reviews.controller.ts
+++ src/apis/reviews/reviews.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    ParseUUIDPipe,
+    Post,
+    Query,
+} from '@nestjs/common'
 import { ReviewsService } from './reviews.service'
 import { CreateReviewWithStore } from './dto/createReview.dto'
 import { Review } from './entities/review.entity'
@@ -21,5 +29,22 @@ export class ReviewsController {
         @Body() createReviewWithStore: CreateReviewWithStore
     ): Promise<Review> {
         return this.reviewsService.create({ ...createReviewWithStore })
+    }
+
+    @Get(':userId')
+    @ApiOperation({
+        summary: '유저가 작성한 리뷰 조회',
+    })
+    @ApiResponse({
+        status: 201,
+        description: '조회 성공.',
+        type: [Review],
+    })
+    fetchReviews(
+        @Param('userId', ParseUUIDPipe) userId: string,
+        @Query('page') page: number
+    ) {
+        console.log(userId)
+        console.log(page)
     }
 }
